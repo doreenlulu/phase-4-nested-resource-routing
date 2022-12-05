401d54bcf5a4c5859f179f9369bddd83e48d7f9b
Doreen <doreenlulu97@gmail.com>
2022-12-05 04:26:09 +0300

    commit tree f89377e0f03243c184f02f5adb84b06e2c4d6041

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,15 +1,36 @@
 class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  def index
+    if params[:dog_house_id]
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else
+      reviews = Review.all
+    end
+    render json: reviews, include: :dog_house
+  end
+
   def show
     dog_house = DogHouse.find(params[:id])
     render json: dog_house
   end
 
-  private
+  # def reviews_index
+  #     dog_house = DogHouse.find(params[:dog_house_id])
+  #     reviews = dog_house.reviews
+  #     render json: reviews, include: :dog_house
+  #   end
 
-  def render_not_found_response
-    render json: { error: "Dog house not found" }, status: :not_found
-  end
+  #   def review
+  #     review = Review.find(params[:id])
+  #     render json: review, include: :dog_house
+  #   end
+
+    private
+
+    def render_not_found_response
+      render json: { error: "Dog house not found" }, status: :not_found
+    end
 
 end
